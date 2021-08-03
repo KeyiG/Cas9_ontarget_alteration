61835eda80b2a33b197105a01e3f63085dd9c175
KeyiG <34516540+KeyiG@users.noreply.github.com>
2021-08-03 09:43:57 +0200

    Update BWA_align_MEM.sh

--- ChIP_seq/BWA_align_MEM.sh
+++ ChIP_seq/BWA_align_MEM.sh
@@ -20,9 +20,11 @@ FQ_PATH=/proj/fastq
 OUTPUT_PATH=/proj/BWA_alignment
 REF_PATH=/proj/reference_hg38
 
+#the ref.fa is the reference sequence. hg38 human reference genome or contig assembled.
+bwa index ${REF_PATH}/ref.fa
 cd ${FQ_PATH}
 for i in $(ls *.fastq.gz | sed 's/.fastq.gz//g'); do
-	bwa mem -t 6 ${REF_PATH}/hg38.fa ${i}.fastq.gz | samtools sort -@ 2 -o ${OUTPUT_PATH}/${i}.bam
+	bwa mem -t 6 ${REF_PATH}/ref.fa ${i}.fastq.gz | samtools sort -@ 2 -o ${OUTPUT_PATH}/${i}.bam
 done
 
 module unload bwa/0.7.17
