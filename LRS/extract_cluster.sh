d839472a03e6a79706a7821703e1e16a15501908
KeyiG <34516540+KeyiG@users.noreply.github.com>
2022-08-11 15:14:01 +0200

    Update extract_cluster.sh

--- LRS/extract_cluster.sh
+++ LRS/extract_cluster.sh
@@ -2,6 +2,9 @@ module load bioinfo-tools
 module load samtools/1.12
 module load BEDTools/2.29.2
 
+#for HAP1 t72, since the reads mapped there are too many, for easy visualization, reads mapped to the three breakpoint are extracted as three cluster. 
+#The bed files of the location are in the same folder, cluster1/2/3.bed.
+
 for i in $(ls *.bam | sed 's/.bam//g'); do \
 	for j in $(ls cluster*.bed | sed 's/.bed//g'); do \
 		bedtools intersect -abam ${i}.bam -b ${j}.bed -u -F 1 > ${i}_${j}.bam
