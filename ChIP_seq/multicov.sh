b934b8f006f6aad7b705f114cc8dae72da33e9f9
KeyiG <34516540+KeyiG@users.noreply.github.com>
2022-08-12 11:50:02 +0200

    Delete multicov.sh

--- ChIP_seq/multicov.sh
+++
@@ -1,5 +0,0 @@
-module load bioinfo-tools
-module load BEDTools
-
-bedtools multicov -D -q 1 -bams *.bam -bed hg38-tRNAs.bed > Cas9_ontarget_pol3_counting.txt
-#*.bam is the bam files of all samples. The bam file has been processed with PCR duplication removal and exluded the reads in the blasklist. 
